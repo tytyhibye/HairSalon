fe2b230f6d4f22779ce9552012cd2fe4a27189db
Tyler Bates <Fondlytyler@gmail.com>
2020-05-29 13:32:36 -0700

    correct routing of foreach loop in stylist details to correctly display reviews.

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -13,7 +13,7 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public virtual ICollection<Review> Reviews {get;set;}
+    public virtual ICollection<Review> Reviews { get; set; }
     public virtual ICollection<Client> Clients { get; set; } // generic interface - collection of method signatures bundled together.
                                                                      // ICollection required by Entity to outline methods for querying and changing data.
   }
