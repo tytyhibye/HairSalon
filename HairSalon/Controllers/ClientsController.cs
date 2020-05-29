302a2c0b74104a402283c9ead40ae5fad94a286a
Tyler Bates <Fondlytyler@gmail.com>
2020-05-29 10:00:16 -0700

    add client controllers for index and create methods.

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client Client)
+    {
+      _db.Clients.Add(Client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+  }
+}
